53accae13c7048f57993bbd72785cf9ec8829eaa
Charon1111 <1437670524@qq.com>
2018-09-02 10:37:49 +0800

    客户端进入仓库

---
+++ vcampus-client/src/RoundBorder.java
@@ -0,0 +1,84 @@
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.border.Border;
+
+public class RoundBorder implements Border{
+	private Color color;
+	 
+	public RoundBorder(Color color) {// 有参数的构造方法
+		this.color = color;
+	}
+ 
+	public RoundBorder() {// 无参构造方法
+		this.color = Color.BLACK;
+		// 如果实例化时，没有传值
+		// 默认是黑色边框
+	}
+	public Insets getBorderInsets(Component c) {
+		return new Insets(0, 0, 0, 0);
+	}
+ 
+	public boolean isBorderOpaque() {
+		return false;
+	}
+ 
+	// 实现Border（父类）方法
+	@Override
+	public void paintBorder(Component c, Graphics g, int x, int y, int width,
+			int height) {
+		g.setColor(color);
+		g.drawRoundRect(0, 0, c.getWidth() - 1, c.getHeight() - 1, 15, 15);
+	}
+}
+/*
+ *  Graphics2D g2d = (Graphics2D) g.create();
+        int h = getHeight();
+        int w = getWidth();
+        float tran = 1F;
+        if (!hover) {
+            tran = 0.3F;
+        }
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint p1;
+        GradientPaint p2;
+        if (getModel().isPressed()) {
+            p1 = new GradientPaint(0, 0, new Color(0, 0, 0), 0, h - 1,
+            		new Color(255, 204, 255));
+                    //new Color(100, 100, 100));
+            p2 = new GradientPaint(0, 1, new Color(0, 0, 0, 50), 0, h - 3,
+                    new Color(255, 255, 255, 100));
+        } else {
+            p1 = new GradientPaint(0, 0, new Color(100, 100, 100), 0, h - 1,
+                    new Color(0, 0, 0));
+            p2 = new GradientPaint(0, 1, new Color(255, 255, 255, 100), 0,
+                    h - 3, new Color(0, 0, 0, 50));
+        }
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+                tran));
+        RoundRectangle2D.Float r2d = new RoundRectangle2D.Float(0, 0, w - 1,
+                h - 1, 20, 20);
+        Shape clip = g2d.getClip();
+        g2d.clip(r2d);
+        GradientPaint gp = new GradientPaint(0.0F, 0.0F, BUTTON_COLOR1, 0.0F,
+                h, BUTTON_COLOR2, true);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, w, h);
+        g2d.setClip(clip);
+        g2d.setPaint(p1);
+        g2d.drawRoundRect(0, 0, w - 1, h - 1, 20, 20);
+        g2d.setPaint(p2);
+        g2d.drawRoundRect(1, 1, w - 3, h - 3, 18, 18);
+        g2d.dispose();
+        super.paintComponent(g);
+ */
